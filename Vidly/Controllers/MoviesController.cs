149ba8f45b843f42d58385f2fabcfd2628b49826
pratik.shrestha <pratik.shrestha@cotiviti.com>
2022-01-14 16:57:24 +0545

    Custom Routes

--- Vidly/Controllers/MoviesController.cs
+++ Vidly/Controllers/MoviesController.cs
@@ -10,14 +10,41 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movie
-        public ActionResult Random()
+        public ViewResult Random()
         {
             var movie = new Movie()
             {
                 Id = 1,
                 Name = "Shrek"
             };
+
             return View(movie);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            return Content("Id = " + id);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+            {
+                pageIndex = 1;
+            }
+            if (String.IsNullOrWhiteSpace(sortBy))
+            {
+                sortBy = "Name";
+            }
+
+
+            return Content(String.Format("pageIndex = {0}, sortBy = {1}", pageIndex, sortBy));
+        }
+
+        
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content(year + "/" + month);
         }
     }
 }
