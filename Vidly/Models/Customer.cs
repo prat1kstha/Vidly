7331ab850477b3b7c3ae59ce08e70c60be76c0ae
pratik.shrestha <pratik.shrestha@cotiviti.com>
2022-01-15 18:22:18 +0545

    Section 3 Exercise

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -11,6 +11,7 @@ namespace Vidly.Models
         public int Id { get; set; }
         [Required, StringLength(255)]
         public string Name { get; set; }
+        public DateTime? BirthDate { get; set; }
         public bool IsSubscribedToNewsLetter { get; set; }
         public byte MembershipTypeId { get; set; }
         public MembershipType MembershipType { get; set; }
--- Vidly/Models/MembershipType.cs
+++ Vidly/Models/MembershipType.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Vidly.Models
 {
     public class MembershipType
     {
         public byte Id { get; set; }
+        public string Name { get; set; }
         public short SignUpFee { get; set; }
         public byte DurationInMonths { get; set; }
         public byte DiscountRate { get; set; }
